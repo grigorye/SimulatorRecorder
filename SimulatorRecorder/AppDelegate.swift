4de198694fdcd371281c09b9ed1da4571ce82ebb
Grigory Entin <grigory.entin@gmail.com>
2018-01-31 22:04:04 +0100

    Initial Commit

---
+++ SimulatorRecorder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimulatorRecorder
+//
+//  Created by Grigory Entin on 31.01.2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
