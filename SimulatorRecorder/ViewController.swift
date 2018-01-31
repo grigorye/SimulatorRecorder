e21ab79eff05942d096e746d46e703a54945d2b3
Grigory Entin <grigory.entin@gmail.com>
2018-01-31 23:56:47 +0100

    First working? version.

--- SimulatorRecorder/ViewController.swift
+++ SimulatorRecorder/ViewController.swift
@@ -9,19 +9,42 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
+	
+	@objc dynamic var process: Process?
+	
+	@IBAction func stopRecording(_ sender: Any) {
+		
+		guard let process = process else {
+			
+			assert(false)
+			return
+		}
+		
+		process.interrupt()
 	}
-
-	override var representedObject: Any? {
-		didSet {
-		// Update the view, if already loaded.
+	
+	@IBAction func newScreenRecording(_ sender: Any) {
+		
+		guard let recorderExecutableURL = Bundle(for: type(of: self)).url(forResource: "recordVideo", withExtension: "") else {
+			
+			assert(false)
+			return
+		}
+		
+		do {
+			let process = try Process.run(recorderExecutableURL, arguments: []) {
+				
+				print("\($0.terminationStatus)")
+				DispatchQueue.main.async {
+					self.process = nil
+				}
+			}
+			self.process = process
+			
+		} catch {
+			
+			NSApplication.shared.presentError(error)
 		}
 	}
-
-
 }
 
