faf2a4bce3521d8bb9eaa574703571ab1ba61bf1
Grigory Entin <grigory.entin@gmail.com>
2018-02-01 21:14:56 +0100

    Added global shortcut handling.

--- SimulatorRecorder/AppDelegate.swift
+++ SimulatorRecorder/AppDelegate.swift
@@ -7,8 +7,46 @@
 //
 
 import Cocoa
+import Carbon.HIToolbox
+
+@objc protocol GlobalActionResponder {
+	
+	func toggleRecording(_ sender: Any)
+}
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 	
+	func keyDown(_ event: NSEvent) {
+		
+		guard event.modifierFlags.intersection([.command, .shift, .option, .control]) == [.command, .shift] else {
+			
+			return
+		}
+		guard event.keyCode == kVK_ANSI_6 else {
+			
+			return
+		}
+		
+		let application = NSApplication.shared
+		
+		guard let viewController = application.windows.first?.contentViewController as? ViewController else {
+			
+			assert(false)
+			return
+		}
+		
+		viewController.toggleRecording(self)
+	}
+	
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		
+		AXIsProcessTrustedWithOptions([kAXTrustedCheckOptionPrompt.takeUnretainedValue() : true] as CFDictionary)
+		
+		NSEvent.addGlobalMonitorForEvents(matching: .keyDown) {
+			
+			self.keyDown($0)
+		}
+	}
 }
