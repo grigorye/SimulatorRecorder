8bbdece05b3ccb6fc3e45ad095a4e62364f2d18e
Grigory Entin <grigory.entin@gmail.com>
2018-02-01 02:50:27 +0100

    Segrgated completeRecording/added process error reporting.

--- SimulatorRecorder/ViewController.swift
+++ SimulatorRecorder/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: NSViewController {
 	
 	@objc dynamic var process: Process?
 	
+	enum RecordingError : Error {
+		case missingRecorderExecutable
+		case badProcessTerminationStatus(Int32)
+	}
+	
 	@IBAction func stopRecording(_ sender: Any) {
 		
 		guard let process = process else {
@@ -23,28 +28,66 @@ class ViewController: NSViewController {
 		process.interrupt()
 	}
 	
-	@IBAction func newScreenRecording(_ sender: Any) {
+	func completeRecording() {
 		
-		guard let recorderExecutableURL = Bundle(for: type(of: self)).url(forResource: "recordVideo", withExtension: "") else {
-			
+		guard let process = process else {
 			assert(false)
 			return
 		}
 		
-		do {
-			let process = try Process.run(recorderExecutableURL, arguments: []) {
-				
-				print("\($0.terminationStatus)")
-				DispatchQueue.main.async {
-					self.process = nil
-				}
-			}
-			self.process = process
+		defer {
+			self.process = nil
+		}
+		
+		let terminationStatus = process.terminationStatus
+		
+		if 0 != terminationStatus {
 			
+			guard let window = view.window else {
+				
+				assert(false)
+				return
+			}
+			
+			presentError(RecordingError.badProcessTerminationStatus(terminationStatus), modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
+		}
+	}
+	
+	func startRecording() throws {
+		
+		guard let recorderExecutableURL = Bundle(for: type(of: self)).url(forResource: "recordVideo", withExtension: "") else {
+			
+			throw RecordingError.missingRecorderExecutable
+		}
+		
+		let process = try Process.run(recorderExecutableURL, arguments: []) {
+			
+			assert(self.process == $0)
+
+			DispatchQueue.main.async {
+				
+				self.completeRecording()
+			}
+		}
+		
+		self.process = process
+	}
+	
+	@IBAction func newScreenRecording(_ sender: Any) {
+		
+		do {
+			
+			try startRecording()
+		
 		} catch {
 			
-			NSApplication.shared.presentError(error)
+			guard let window = view.window else {
+				
+				assert(false)
+				return
+			}
+			
+			presentError(error, modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
 		}
 	}
 }
-
