74583f60f6e5a3a4daa861d58c7406cdfc7666eb
Grigory Entin <grigory.entin@gmail.com>
2018-02-03 00:18:19 +0100

    Squashed 'GETracing/' changes from c9bc65d..2115631
    2115631 Switched to @_exported.
    
    git-subtree-dir: GETracing
    git-subtree-split: 2115631a734aef94fc3c89b1bdd75214b439239d

--- GETracing/ModuleExports-GETracing.swift
+++ GETracing/ModuleExports-GETracing.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2016 Grigory Entin. All rights reserved.
 //
 
-import GETracing
+@_exported import GETracing
+
 import Foundation
 
 internal var moduleTracingEnabled: Bool = {
@@ -22,17 +23,4 @@ internal func x$<T>(file: String = #file, line: Int = #line, column: Int = #colu
 		return try valueClosure()
 	}
 	return try GETracing.x$(file: file, line: line, column: column, function: function, dso: dso, valueClosure)
-}
-
-internal prefix func •<T>(argument: @autoclosure () -> T) {
-	// swiftlint:disable:previous identifier_name
-}
-
-internal func L<T>(file: String = #file, line: Int = #line, column: Int = #column, function: String = #function, dso: UnsafeRawPointer = #dsohandle, _ valueClosure: @autoclosure () -> T) -> String {
-	// swiftlint:disable:previous identifier_name
-	return GETracing.L(file: file, line: line, column: column, function: function, dso: dso, valueClosure)
-}
-
-internal func disableTrace(file: String = #file, function: String = #function) -> Any? {
-	return GETracing.disableTrace(file: file, function: function)
 }
