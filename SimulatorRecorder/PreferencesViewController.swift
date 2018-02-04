be7bd12cdab804f7fad0fd0cedccee857ad0db6b
Grigory Entin <grigory.entin@gmail.com>
2018-02-04 09:29:53 +0100

    Added token palette.

--- SimulatorRecorder/PreferencesViewController.swift
+++ SimulatorRecorder/PreferencesViewController.swift
@@ -8,16 +8,51 @@
 
 import Cocoa
 
+private let tokensInPalette: [FilenameToken] = [
+	.date,
+	.time,
+	.version,
+	.device
+]
+
 class PreferencesViewController : NSViewController {
 	
-	@IBOutlet var filenamePaletteTokenField: NSTokenField!
 	@IBOutlet var filenameTokenField: NSTokenField!
+	@IBOutlet var filenameTokenGridView: NSGridView!
+	
+	let filenamePaletteTokenFieldDelegate = FilenamePaletteTokenFieldDelegate()
 	
 	override func viewDidLoad() {
 		
 		super.viewDidLoad()
 		
-		filenameTokenField.objectValue = ["Recording", FilenameTokenObject(value: .date), "at", FilenameTokenObject(value: .time), "for", FilenameTokenObject(value: .device), "-", FilenameTokenObject(value: .version)]
-		filenamePaletteTokenField.objectValue = ([.date, .time, .device, .version] as [FilenameToken]).map(FilenameTokenObject.init(value:))
+		filenameTokenField.objectValue = [
+			"Recording",
+			FilenameTokenObject(.date),
+			"at",
+			FilenameTokenObject(.time),
+			"for",
+			FilenameTokenObject(.device),
+			"-",
+			FilenameTokenObject(.version)
+		]
+
+		filenameTokenGridView … {
+			$0.rowSpacing = 10
+			$0.columnSpacing = 10
+		}
+		for token in tokensInPalette {
+			filenameTokenGridView.addRow(with: [
+				NSTextField(labelWithString: token.title),
+				NSTokenField() … {
+					$0.isBordered = false
+					$0.isSelectable = true
+					$0.isEditable = false
+					$0.drawsBackground = false
+					$0.objectValue = [token].map(FilenameTokenObject.init)
+					$0.delegate = filenamePaletteTokenFieldDelegate
+				}
+			])
+		}
 	}
 }
