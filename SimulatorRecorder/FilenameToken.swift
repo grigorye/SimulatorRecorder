c0945441659b221c365f934a2b03ebc0ea18aad4
Grigory Entin <grigory.entin@gmail.com>
2018-02-04 09:29:33 +0100

    Segregated FilenameToken.swift; added .title/.sampleString.

---
+++ SimulatorRecorder/FilenameToken.swift
@@ -0,0 +1,52 @@
+//
+//  FilenameToken.swift
+//  SimulatorRecorder
+//
+//  Created by Grigory Entin on 03.02.2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+enum FilenameToken : String, RawRepresentable {
+	
+	case date
+	case time
+	case version
+	case device
+}
+
+private let now = Date()
+
+extension FilenameToken {
+	
+	var title : String {
+		
+		switch self {
+			
+		case .date:
+			return NSLocalizedString("Date", comment: "")
+		case .time:
+			return NSLocalizedString("Time", comment: "")
+		case .device:
+			return NSLocalizedString("Device", comment: "")
+		case .version:
+			return NSLocalizedString("System Version", comment: "")
+		}
+	}
+
+	var sampleString: String {
+		
+		switch self {
+			
+		case .date:
+			return DateFormatter.localizedString(from: now, dateStyle: .short, timeStyle: .none)
+		case .device:
+			return "iPhone SE"
+		case .version:
+			return "11.2"
+		case .time:
+			return DateFormatter.localizedString(from: now, dateStyle: .none, timeStyle: .short)
+		}
+	}
+}
