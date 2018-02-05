ca2773b2dd37fbb2afbe4f5d3a05df958a75547a
Grigory Entin <grigory.entin@gmail.com>
2018-02-05 23:03:46 +0100

    Added PathFromFilePathOrURLTransformer.swift.

---
+++ SimulatorRecorder/PathFromFilePathOrURLTransformer.swift
@@ -0,0 +1,53 @@
+//
+//  PathFromFilePathOrURLTransformer.swift
+//  SimulatorRecorder
+//
+//  Created by Grigory Entin on 05.02.2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+extension NSValueTransformerName {
+	
+	static let pathFromFilePathOrURLTransformerName = NSValueTransformerName(rawValue: "PathFromFilePathOrURL")
+}
+
+/// Transforms (String|URL*) <-> String (favoring URLs); suitable for binding NSPathControl's value to user defaults.
+class PathFromFilePathOrURLTransformer : ValueTransformer {
+
+	override class func transformedValueClass() -> AnyClass {
+		
+		return NSURL.self
+	}
+	
+	override class func allowsReverseTransformation() -> Bool {
+		
+		return true
+	}
+	
+	override func transformedValue(_ value: Any?) -> Any? {
+		
+		guard let path = value as? String else {
+			
+			return value
+		}
+		let url = URL(fileURLWithPath: path)
+		return url
+	}
+	
+	override func reverseTransformedValue(_ value: Any?) -> Any? {
+		
+		switch value {
+		case nil:
+			return nil
+		case let url as URL:
+			return url.path
+		case let path as String:
+			return path
+		default:
+			assert(false)
+			return value
+		}
+	}
+}
