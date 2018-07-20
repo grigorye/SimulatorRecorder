f7529a35664819577b81392fc7b84e2a6896d20f
Grigory Entin <grigory.entin@gmail.com>
2018-07-21 00:39:07 +0200

    Segregated RecordingController; fixed termination/interruption handling.

---
+++ SimulatorRecorder/RecordingController.swift
@@ -0,0 +1,103 @@
+//
+//  RecordingController.swift
+//  SimulatorRecorder
+//
+//  Created by Entin, Grigorii on 18/07/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import Foundation
+
+@objc class RecordingController : NSObject {
+	
+    @objc dynamic var process: Process?
+    
+    enum RecordingError : Error {
+        case missingRecorderExecutable
+        case badProcessTerminationStatus(Int32)
+    }
+    
+    
+    @objc dynamic var readyToRecord: Bool {
+        return process == nil
+    }
+    @objc dynamic class var keyPathsForValuesAffectingReadyToRecord: Set<String> {
+        return [
+            #keyPath(process)
+        ]
+    }
+    
+    @objc dynamic var recording: Bool {
+        return !readyToRecord && !interrupting && !terminated
+    }
+    @objc dynamic class var keyPathsForValuesAffectingRecording: Set<String> {
+        return [
+            #keyPath(readyToRecord),
+			#keyPath(interrupting),
+			#keyPath(terminated)
+        ]
+    }
+    
+    @objc dynamic var interrupting: Bool = false
+	@objc dynamic var terminated: Bool = false
+    
+    func stopRecording() {
+        guard let process = process else {
+            assert(false)
+            return
+        }
+        
+        assert(!interrupting)
+		interrupting = true
+        process.interrupt()
+		process.waitUntilExit()
+		interrupting = false
+    }
+    
+    func completeRecording(completionHandler: (Error?) -> Void) {
+        guard let process = process else {
+            assert(false)
+            return
+        }
+        
+        defer {
+            self.process = nil
+            interrupting = false
+        }
+        
+        let terminationStatus = process.terminationStatus
+        
+        guard 0 == terminationStatus else {
+			let error = RecordingError.badProcessTerminationStatus(terminationStatus)
+            completionHandler(error)
+			return
+        }
+		
+		completionHandler(nil)
+    }
+	
+	func recorderExecutableURL() throws -> URL {
+		guard let recorderExecutableURL = Bundle(for: type(of: self)).url(forResource: "recordVideo", withExtension: "") else {
+			
+			throw RecordingError.missingRecorderExecutable
+		}
+		return recorderExecutableURL
+	}
+	
+	func startRecording(terminationHandler: @escaping ((Error?) -> Void)) {
+		do {
+			let recorderExecutableURL = try self.recorderExecutableURL()
+			
+			let process = try Process.run(recorderExecutableURL, arguments: []) { (process) in
+				DispatchQueue.main.async {
+					assert(self.process == process)
+					self.completeRecording(completionHandler: terminationHandler)
+				}
+			}
+
+        	self.process = process
+		} catch {
+			terminationHandler(error)
+		}
+    }
+}
--- SimulatorRecorder/ViewController.swift
+++ SimulatorRecorder/ViewController.swift
@@ -8,140 +8,52 @@
 
 import Cocoa
 
-class ViewController: NSViewController, GlobalActionResponder {
+class ViewController : NSViewController, GlobalActionResponder {
 	
-	@objc dynamic var process: Process?
+	@objc dynamic var recordingController: RecordingController! = RecordingController()
 	
-	enum RecordingError : Error {
-		case missingRecorderExecutable
-		case badProcessTerminationStatus(Int32)
+	@IBAction func stopRecording(_ sender: Any) {
+		recordingController.stopRecording()
 	}
 	
-	override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-		
-		switch menuItem.action {
-			
-		case #selector(stopRecording(_:))?:
-			
-			return recording
-			
-		case #selector(newScreenRecording(_:))?:
-			
-			return readyToRecord
-			
-		default:
-			return true
-		}
-	}
-	
-    @objc dynamic var readyToRecord: Bool {
-        
-        return process == nil
-    }
-    @objc dynamic class var keyPathsForValuesAffectingReadyToRecord: Set<String> {
-        
-        return [
-            #keyPath(process)
-        ]
-    }
-
-	@objc dynamic var recording: Bool {
-		
-		return !readyToRecord && !interrupting
-	}
-    @objc dynamic class var keyPathsForValuesAffectingRecording: Set<String> {
-        
-        return [
-            #keyPath(readyToRecord),
-            #keyPath(interrupting)
-        ]
-    }
-
-    @objc dynamic var interrupting: Bool = false
-    
 	@IBAction func toggleRecording(_ sender: Any) {
-		
-		if recording {
+		if recordingController.recording {
 			stopRecording(sender)
 		} else {
 			newScreenRecording(sender)
 		}
 	}
 	
-	@IBAction func stopRecording(_ sender: Any) {
-		
-		guard let process = process else {
-			
+	func completeRecording(_ error: Error?) {
+		guard let window = view.window else {
 			assert(false)
 			return
 		}
 		
-        assert(!interrupting)
-        interrupting = true
-		process.interrupt()
-	}
-	
-	func completeRecording() {
-		
-		guard let process = process else {
-			assert(false)
+		guard let error = error else {
 			return
 		}
 		
-		defer {
-			self.process = nil
-            interrupting = false
-		}
-		
-		let terminationStatus = process.terminationStatus
-		
-		if 0 != terminationStatus {
-			
-			guard let window = view.window else {
-				
-				assert(false)
-				return
-			}
-			
-			presentError(RecordingError.badProcessTerminationStatus(terminationStatus), modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
-		}
-	}
-	
-	func startRecording() throws {
-		
-		guard let recorderExecutableURL = Bundle(for: type(of: self)).url(forResource: "recordVideo", withExtension: "") else {
-			
-			throw RecordingError.missingRecorderExecutable
-		}
-		
-		let process = try Process.run(recorderExecutableURL, arguments: []) {
-			
-			assert(self.process == $0)
-			
-			DispatchQueue.main.async {
-				
-				self.completeRecording()
-			}
-		}
-		
-		self.process = process
+		presentError(error, modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
 	}
 	
 	@IBAction func newScreenRecording(_ sender: Any) {
-		
-		do {
+		recordingController.startRecording { (error) in
+			self.completeRecording(error)
+		}
+	}
+	
+	override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+		switch menuItem.action {
 			
-			try startRecording()
+		case #selector(stopRecording(_:))?:
+			return recordingController.recording
 			
-		} catch {
+		case #selector(newScreenRecording(_:))?:
+			return recordingController.readyToRecord
 			
-			guard let window = view.window else {
-				
-				assert(false)
-				return
-			}
-			
-			presentError(error, modalFor: window, delegate: nil, didPresent: nil, contextInfo: nil)
+		default:
+			return true
 		}
 	}
 }
