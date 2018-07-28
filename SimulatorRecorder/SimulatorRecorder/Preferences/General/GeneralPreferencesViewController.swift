dc0c578d23bda9c5ac1882fa9ccc51d155d95279
Grigory Entin <grigory.entin@gmail.com>
2018-07-28 21:29:47 +0200

    Added GeneralPreferencesViewController.

---
+++ SimulatorRecorder/SimulatorRecorder/Preferences/General/GeneralPreferencesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GeneralPreferencesViewController.swift
+//  SimulatorRecorder
+//
+//  Created by Grigory Entin on 28/07/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import AppKit
+
+class GeneralPreferencesViewController : NSViewController {
+	
+	@IBOutlet var userDefaultsController: NSUserDefaultsController!
+
+}
