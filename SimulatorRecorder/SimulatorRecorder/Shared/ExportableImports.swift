d6e56a86e085a7dea4749fa09596e980726bfeb7
Grigory Entin <grigory.entin@gmail.com>
2018-08-05 19:14:25 +0200

    Added ExportableImports.swift.

---
+++ SimulatorRecorder/SimulatorRecorder/Shared/ExportableImports.swift
@@ -0,0 +1,10 @@
+//
+//  ExportableImports.swift
+//  SimulatorRecorder
+//
+//  Created by Grigory Entin on 05/08/2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+@_exported import GEFoundation
+@_exported import GETracing
