1f990c8cb310e75e44b53218627404e73618f84b
Grigory Entin <grigory.entin@gmail.com>
2018-08-05 21:14:28 +0200

    Removed GEFoundation in favour of CocoaPods variant.

--- GEFoundation/GEFoundation/ModuleExports-GEFoundation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ModuleExports-GEFoundation.swift
-//  GEFoundation
-//
-//  Created by Grigory Entin on 09.12.16.
-//  Copyright © 2016 Grigory Entin. All rights reserved.
-//
-
-@_exported import GEFoundation
