3c4168e1fca254f25515d509e1defaea9f7cf5f4
Grigory Entin <grigory.entin@gmail.com>
2018-08-18 17:11:51 +0200

    Switched to lazy binding.

--- SimulatorRecorder/SimulatorRecorder/Recording/RecordingController.swift
+++ SimulatorRecorder/SimulatorRecorder/Recording/RecordingController.swift
@@ -63,42 +63,85 @@ import Foundation
 	
 	// MARK: -
 	
-	override init() {
-		super.init()
-		
-		self.bind(
-			NSBindingName(rawValue: #keyPath(recording)),
-			to: deviceRecordersController,
-			withKeyPath: arrangedObjectsKeyPath(.max, \DeviceRecorder.recording),
-			options: [
-				.nullPlaceholder: false
-			]
-		)
-		self.bind(
-			NSBindingName(rawValue: #keyPath(readyToRecord)),
-			to: deviceRecordersController,
-			withKeyPath: arrangedObjectsKeyPath(.min, \DeviceRecorder.readyToRecord),
-			options: [
-				.nullPlaceholder: true
-			]
-		)
-		self.bind(
-			NSBindingName(rawValue: #keyPath(interrupting)),
-			to: deviceRecordersController,
-			withKeyPath: arrangedObjectsKeyPath(.max, \DeviceRecorder.interrupting),
-			options: [
-				.nullPlaceholder: false
-			]
-		)
-	}
-	
 	@objc dynamic var deviceRecordersController = NSArrayController()
 	@objc dynamic var deviceRecorders: [DeviceRecorder] = [] {
 		didSet {
 			deviceRecordersController.content = deviceRecorders
 		}
 	}
-	@objc dynamic var recording: Bool = false
-	@objc dynamic var readyToRecord: Bool = false
-	@objc dynamic var interrupting: Bool = false
+
+	// MARK: -
+	
+	@objc dynamic class var keyPathsForValuesAffectingRecording: Set<String> {
+		return [#keyPath(recordingImp)]
+	}
+	private lazy var recordingBinding: Void = {
+		self.bind(
+			NSBindingName(rawValue: #keyPath(recordingImp)),
+			to: deviceRecordersController,
+			withKeyPath: arrangedObjectsKeyPath(.max, \DeviceRecorder.recording),
+			options: [
+				.nullPlaceholder: false
+			]
+		)
+	}()
+	@objc dynamic var recording: Bool {
+		_ = self.recordingBinding
+		return recordingImp
+	}
+	@objc private dynamic var recordingImp: Bool = false {
+		willSet {
+			_ = x$(newValue)
+		}
+	}
+
+	// MARK: -
+	
+	@objc dynamic class var keyPathsForValuesAffectingReadyToRecord: Set<String> {
+		return [#keyPath(readyToRecordImp)]
+	}
+	private lazy var readyToRecordBinding: Void = {
+		self.bind(
+			NSBindingName(rawValue: #keyPath(readyToRecordImp)),
+			to: deviceRecordersController,
+			withKeyPath: arrangedObjectsKeyPath(.min, \DeviceRecorder.readyToRecord),
+			options: [
+				.nullPlaceholder: true
+			]
+		)
+	}()
+	@objc dynamic var readyToRecord: Bool {
+		_ = readyToRecordBinding
+		return readyToRecordImp
+	}
+	@objc private dynamic var readyToRecordImp: Bool = false {
+		willSet {
+			_ = x$(newValue)
+		}
+	}
+	
+	// MARK: -
+	
+	@objc dynamic class var keyPathsForValuesAffectingInterrupting: Set<String> {
+		return [#keyPath(interruptingImp)]
+	}
+	private lazy var interruptingBinding: Void = {
+		self.bind(
+			NSBindingName(rawValue: #keyPath(interruptingImp)),
+			to: deviceRecordersController,
+			withKeyPath: arrangedObjectsKeyPath(.max, \DeviceRecorder.interrupting),
+			options: [
+				.nullPlaceholder: false
+			]
+		)
+	}()
+	@objc dynamic var interrupting: Bool {
+		_ = interruptingBinding
+		return interruptingImp
+	}
+	@objc private dynamic var interruptingImp: Bool = false {
+		willSet {
+			_ = x$(newValue)
+		}
+	}
 }
